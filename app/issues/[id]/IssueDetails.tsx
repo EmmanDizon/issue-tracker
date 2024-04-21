36cb11f408adfd9de3e08174f5bf5fcd20e17460
Emman-Jay-Dizon <emman.jay_dizon@questronix.com.ph>
2024-04-22 00:45:47 +0800

    organize import and refactor coding structure

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,25 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import { Issues } from "@prisma/client";
+import { Heading, Flex, Card, Text } from "@radix-ui/themes";
+import ReactMarkDown from "react-markdown";
+
+interface Props {
+  issue: Issues;
+}
+
+const IssueDetails = ({ issue }: Props) => {
+  return (
+    <>
+      <Heading>{issue.title}</Heading>
+      <Flex gap="3" my="2">
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className="prose" mt="4">
+        <ReactMarkDown>{issue.description}</ReactMarkDown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
