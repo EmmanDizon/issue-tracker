760edb2444862bdf9958b58e8cd57904d64b59a8
Emman-Jay-Dizon <emman.jay_dizon@questronix.com.ph>
2024-04-26 01:09:20 +0800

    card summary component for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 interface Props {
   searchParams: { page: string };
 }
 
-export default function Home({ searchParams }: Props) {
-  return <LatestIssues />;
+export default async function Home({ searchParams }: Props) {
+  const openIssue = await prisma.issues.count({ where: { status: "OPEN" } });
+
+  const inProgress = await prisma.issues.count({
+    where: { status: "IN_PROGRESS" },
+  });
+
+  const closed = await prisma.issues.count({
+    where: { status: "CLOSED" },
+  });
+
+  return (
+    <IssueSummary open={openIssue} inProgress={inProgress} closed={closed} />
+  );
 }
